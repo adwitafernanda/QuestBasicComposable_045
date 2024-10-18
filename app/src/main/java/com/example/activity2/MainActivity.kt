ae26b1f6b6e6e1fceeff198f339dd17010a753c3
adwitafernanda <adwitafernanda4@gmail.com>
2024-10-18 19:01:13 +0700

    menambahkan logo umy

--- app/src/main/java/com/example/activity2/MainActivity.kt
+++ app/src/main/java/com/example/activity2/MainActivity.kt
@@ -4,19 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.activity2.ui.theme.Activity2Theme
 
@@ -44,17 +48,21 @@ fun BasicCompose(modifier: Modifier = Modifier){
          modifier = Modifier.fillMaxSize()) {
          Text(text = "LOGIN",
              modifier= modifier,
-             color = Color.Green,
+             color = Color.Gray,
              fontSize = (80.sp),
              fontWeight = FontWeight.Bold
          )
          Text(text = "Ini Adalah Halaman Login",
              modifier= modifier,
-             color = Color.Blue,
+             color = Color.Black,
              fontSize = (10.sp),
              fontStyle = FontStyle.Italic,
-
          )
+         Image(painter = painterResource(id = R.drawable.logo),
+             contentDescription = null,
+             modifier = Modifier.size(330.dp)
+         )
+
      }
 }
 
