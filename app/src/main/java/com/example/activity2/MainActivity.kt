90ea8e520ddc4734031363c34bc48f9083cd3fc2
adwitafernanda <adwitafernanda4@gmail.com>
2024-10-18 18:48:25 +0700

    meletakkan text pada tengah hp

--- app/src/main/java/com/example/activity2/MainActivity.kt
+++ app/src/main/java/com/example/activity2/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontStyle
@@ -38,7 +39,9 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun BasicCompose(modifier: Modifier = Modifier){
      Column(
-         verticalArrangement = Arrangement.Center,) {
+         verticalArrangement = Arrangement.Center,
+         horizontalAlignment = Alignment.CenterHorizontally,
+         modifier = Modifier.fillMaxSize()) {
          Text(text = "LOGIN",
              modifier= modifier,
              color = Color.Green,
